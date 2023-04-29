7bce148bc31027a1b8ffe6505ce9515e5a7a5b4f
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-30 00:12:38 +0700

    feat: create user data

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'slug',
+        'contact',
+        'address',
+        'description',
         'email',
         'password',
     ];
