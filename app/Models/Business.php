f82a35992abb0237d09e31dfca51d8e366dc8026
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-30 20:20:26 +0700

    feat: install spatie-medialibrary

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -4,15 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Business extends Model
+class Business extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $guarded = ["id"];
 
     public function User()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection("business-permission-letter");
+        $this->addMediaCollection("business-galleries");
     }
 }
