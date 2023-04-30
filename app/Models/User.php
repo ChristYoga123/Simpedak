cbf92e42e25e1f7b9e119b22afa273461dcc4d90
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-30 13:14:56 +0700

    feat: create product owner data

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Business::class);
     }
+
+    public function ProductOwners()
+    {
+        return $this->hasMany(ProductOwner::class);
+    }
 }
