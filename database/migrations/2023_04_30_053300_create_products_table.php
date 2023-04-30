7be2f234450bb4db8609c0d038d965b68bfb99bb
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-30 12:39:22 +0700

    feat: create product data and fix migration

--- database/migrations/2023_04_30_053300_create_products_table.php
+++ database/migrations/2023_04_30_053300_create_products_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('businesses', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained();
             $table->string("name");
             $table->string("slug");
-            $table->string("contact");
-            $table->string("address");
-            $table->longText("description");
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('businesses');
+        Schema::dropIfExists('products');
     }
 };
